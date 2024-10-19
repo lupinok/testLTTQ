50ecea87b9575025749b6fe47f67a0a73700e3f6
lupinok <151240627+lupinok@users.noreply.github.com>
2024-10-19 18:49:10 +0700

    lại

--- testinterface.cs
+++ testinterface.cs
@@ -1,8 +1,36 @@
 ﻿using System;
-
-public class Class1
+public interface IEmployeeBUS
 {
-	public Class1()
-	{
-	}
+    void AddEmployee(EmployeeDTO employee);
+    void UpdateEmployee(EmployeeDTO employee);
+    DataTable GetAllEmployees();
 }
+public class EmployeeBUS : IEmployeeBUS
+{
+    private IEmployeeDAO employeeDAO;
+
+    public EmployeeBUS(IEmployeeDAO employeeDAO)
+    {
+        this.employeeDAO = employeeDAO;
+    }
+
+    public void AddEmployee(EmployeeDTO employee)
+    {
+        // Thêm nhân viên vào cơ sở dữ liệu
+        employeeDAO.InsertEmployee(employee);
+    }
+
+    public void UpdateEmployee(EmployeeDTO employee)
+    {
+        employeeDAO.UpdateEmployee(employee);
+    }
+
+    public DataTable GetAllEmployees()
+    {
+        return employeeDAO.GetAllEmployees();
+    }
+}
+
+
+
+
