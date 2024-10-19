3241ccd38e9ca627b66d86a28937ead326e846cb
lupinok <151240627+lupinok@users.noreply.github.com>
2024-10-19 18:43:19 +0700

    day

---
+++ testkethua.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class testkethua : Form
+{
+    protected void ShowMessage(string message)
+    {
+        MessageBox.Show(message);
+    }
+
+    // Các phương thức chung khác cho các form có thể được định nghĩa ở đây.
+}
+public class EmployeeForm : testkethua
+{
+    private DataGridView dataGridView;
+    private IEmployeeBUS employeeBUS;
+
+    public EmployeeForm(IEmployeeBUS employeeBUS)
+    {
+        this.employeeBUS = employeeBUS;
+
+        InitializeComponent();
+        LoadEmployeeData();
+    }
+
+    private void InitializeComponent()
+    {
+        dataGridView = new DataGridView();
+        this.Controls.Add(dataGridView);
+        dataGridView.Dock = DockStyle.Fill;
+        this.Text = "Danh Sách Nhân Viên";
+    }
+
+    private void LoadEmployeeData()
+    {
+        try
+        {
+            DataTable employees = employeeBUS.GetAllEmployees();
+            dataGridView.DataSource = employees;
+        }
+        catch (Exception ex)
+        {
+            ShowMessage("Lỗi khi tải dữ liệu: " + ex.Message);
+        }
+    }
+}
